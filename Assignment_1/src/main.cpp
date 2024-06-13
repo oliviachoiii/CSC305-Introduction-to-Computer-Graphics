b9d64510142e31f2f0a4f24089c04bdfaa725e20
oliviachoiii <oliviachoi612@gmail.com>
2024-06-13 12:37:05 -0700

    Assignment2

--- Assignment_1/src/main.cpp
+++ Assignment_1/src/main.cpp
@@ -11,20 +11,41 @@
 using namespace Eigen;
 ////////////////////////////////////////////////////////////////////////////////
 
+// Problem message "identifier "DATA_DIR" is undefined"
+#ifndef DATA_DIR
+#define DATA_DIR ""
+#endif
+
 const std::string root_path = DATA_DIR;
 
 // Computes the determinant of the matrix whose columns are the vector u and v
 double inline det(const Vector2d &u, const Vector2d &v)
 {
-    // TODO
-    return 0;
+    Matrix2d mat;
+    mat << u, v;
+    double determinant = mat.determinant();
+    return determinant;
 }
 
 // Return true iff [a,b] intersects [c,d]
 bool intersect_segment(const Vector2d &a, const Vector2d &b, const Vector2d &c, const Vector2d &d)
 {
-    // TODO
-    return true;
+    auto orientation = [](const Vector2d &p, const Vector2d &q, const Vector2d &r) {
+        double val = det(q - p, r - p);
+        if (val == 0) return 0;  // 0: Collinear
+        return (val > 0) ? 1 : 2; // 1: Clockwise 2: Counterclock wise
+    };
+
+    int o1 = orientation(a, b, c);
+    int o2 = orientation(a, b, d);
+    int o3 = orientation(c, d, a);
+    int o4 = orientation(c, d, b);
+
+    // General case
+    if (o1 != o2 && o3 != o4)
+        return true;
+
+    return false;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -32,11 +53,31 @@ bool intersect_segment(const Vector2d &a, const Vector2d &b, const Vector2d &c, 
 bool is_inside(const std::vector<Vector2d> &poly, const Vector2d &query)
 {
     // 1. Compute bounding box and set coordinate of a point outside the polygon
-    // TODO
     Vector2d outside(0, 0);
+    double min_x = poly[0].x();
+    double min_y = poly[0].y();
+    double max_x = poly[0].x();
+    double max_y = poly[0].y();
+
+    for (const auto& point : poly) {
+        if (point.x() < min_x) min_x = point.x();
+        if (point.y() < min_y) min_y = point.y();
+        if (point.x() > max_x) max_x = point.x();
+        if (point.y() > max_y) max_y = point.y();
+    }
+    outside = Vector2d(max_x + 1, max_y + 1);
+
     // 2. Cast a ray from the query point to the 'outside' point, count number of intersections
-    // TODO
-    return true;
+    int count = 0;
+    for (size_t i = 0; i < poly.size(); ++i) {
+        const Vector2d &a = poly[i];
+        const Vector2d &b = poly[(i + 1) % poly.size()];
+        if (intersect_segment(query, outside, a, b))
+            count++;
+    }
+
+    return count % 2 == 1;
+    //return true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -45,13 +86,36 @@ std::vector<Vector2d> load_xyz(const std::string &filename)
 {
     std::vector<Vector2d> points;
     std::ifstream in(filename);
-    // TODO
+    // When it's undable to open file
+    if (!in) {
+        std::cerr << "Unable to open file " << filename << std::endl;
+        return points;
+    }
+    // Read the number of points and read each point's coordinates
+    int n;
+    in >> n;
+    points.reserve(n);
+    for (int i = 0; i < n; ++i) {
+        double x, y, z;
+        in >> x >> y >> z;
+        points.push_back(Vector2d(x, y));
+    }
     return points;
 }
 
 void save_xyz(const std::string &filename, const std::vector<Vector2d> &points)
 {
-    // TODO
+    std::ofstream out(filename);
+    if (!out) {
+        std::cerr << "Unable to open file " << filename << std::endl;
+        return;
+    }
+    // Saves points to the file
+    // Writes the number of points and each point's coordinates
+    out << points.size() << "\n";
+    for (const auto &point : points) {
+        out << point.x() << " " << point.y() << " 0\n";
+    }
 }
 
 std::vector<Vector2d> load_obj(const std::string &filename)
--- Assignment_2/src/main.cpp
+++ Assignment_2/src/main.cpp
@@ -13,6 +13,59 @@
 // Shortcut to avoid Eigen:: everywhere, DO NOT USE IN .h
 using namespace Eigen;
 
+// Function to get the intersection point of a ray with a sphere
+Vector3d getPointSphere(const Vector3d& sphere_center, const Vector3d& ray_origin, const Vector3d& ray_direction, double sphere_radius)
+{
+    double A = ray_direction.dot(ray_direction);
+    double B = 2.0 * ray_direction.dot(ray_origin - sphere_center);
+    double C = (ray_origin - sphere_center).dot(ray_origin - sphere_center) - (sphere_radius * sphere_radius);
+
+    double discriminant = (B * B) - (4 * A * C);
+    double t;
+
+    if (discriminant >= 0)
+    {
+        double t1 = (-B + sqrt(discriminant)) / (2.0 * A);
+        double t2 = (-B - sqrt(discriminant)) / (2.0 * A);
+        t = (t1 < t2 && t1 >= 0) ? t1 : t2;
+    }
+
+    return ray_origin + t * ray_direction;
+}
+
+// Function to check if a ray intersects with a sphere
+bool raysphere(const Vector3d& sphere_center, const Vector3d& ray_origin, const Vector3d& ray_direction, double sphere_radius)
+{
+    double A = ray_direction.dot(ray_direction);
+    double B = 2.0 * ray_direction.dot(ray_origin - sphere_center);
+    double C = (ray_origin - sphere_center).dot(ray_origin - sphere_center) - (sphere_radius * sphere_radius);
+
+    double discriminant = (B * B) - (4 * A * C);
+
+    return discriminant >= 0;
+}
+
+// Function to get the intersection point of a ray with a parallelogram
+Vector3d getPoint(const Vector3d& u_vector, const Vector3d& v_vector, const Vector3d& d_vector, const Vector3d& a_vector, const Vector3d& e_vector)
+{
+    Matrix3d A;
+    A << -u_vector, -v_vector, d_vector;
+    Vector3d ae_vector = a_vector - e_vector;
+    Vector3d solution_vector = A.colPivHouseholderQr().solve(ae_vector);
+    return a_vector + solution_vector(0) * u_vector + solution_vector(1) * v_vector;
+}
+
+// Function to check if a ray intersects with a parallelogram
+bool raytri(const Vector3d& u_vector, const Vector3d& v_vector, const Vector3d& d_vector, const Vector3d& a_vector, const Vector3d& e_vector)
+{
+    Matrix3d A;
+    A << -u_vector, -v_vector, d_vector;
+    Vector3d ae_vector = a_vector - e_vector;
+    Vector3d solution_vector = A.colPivHouseholderQr().solve(ae_vector);
+
+    return solution_vector(2) >= 0 && solution_vector(0) >= 0 && solution_vector(0) <= 1 && solution_vector(1) >= 0 && solution_vector(1) <= 1;
+}
+
 void raytrace_sphere()
 {
     std::cout << "Simple ray tracer, one sphere with orthographic projection" << std::endl;
@@ -44,7 +97,6 @@ void raytrace_sphere()
             const Vector3d ray_direction = camera_view_direction;
 
             // Intersect with the sphere
-            // NOTE: this is a special case of a sphere centered in the origin and for orthographic rays aligned with the z axis
             Vector2d ray_on_xy(ray_origin(0), ray_origin(1));
             const double sphere_radius = 0.9;
 
@@ -108,15 +160,14 @@ void raytrace_parallelogram()
             const Vector3d ray_origin = pixel_center;
             const Vector3d ray_direction = camera_view_direction;
 
-            // TODO: Check if the ray intersects with the parallelogram
-            if (true)
+            // Check if the ray intersects with the parallelogram
+            if (raytri(pgram_u, pgram_v, ray_direction, pgram_origin, ray_origin))
             {
-                // TODO: The ray hit the parallelogram, compute the exact intersection
-                // point
-                Vector3d ray_intersection(0, 0, 0);
+                // The ray hit the parallelogram, compute the exact intersection point
+                Vector3d ray_intersection = getPoint(pgram_u, pgram_v, ray_direction, pgram_origin, ray_origin);
 
-                // TODO: Compute normal at the intersection point
-                Vector3d ray_normal = ray_intersection.normalized();
+                // Compute normal at the intersection point
+                Vector3d ray_normal = pgram_v.cross(pgram_u).normalized();
 
                 // Simple diffuse model
                 C(i, j) = (light_position - ray_intersection).normalized().transpose() * ray_normal;
@@ -150,7 +201,7 @@ void raytrace_perspective()
     const Vector3d x_displacement(2.0 / C.cols(), 0, 0);
     const Vector3d y_displacement(0, -2.0 / C.rows(), 0);
 
-    // TODO: Parameters of the parallelogram (position of the lower-left corner + two sides)
+    // Parameters of the parallelogram (position of the lower-left corner + two sides)
     const Vector3d pgram_origin(-0.5, -0.5, 0);
     const Vector3d pgram_u(0, 0.7, -10);
     const Vector3d pgram_v(1, 0.4, 0);
@@ -164,18 +215,18 @@ void raytrace_perspective()
         {
             const Vector3d pixel_center = image_origin + double(i) * x_displacement + double(j) * y_displacement;
 
-            // TODO: Prepare the ray (origin point and direction)
-            const Vector3d ray_origin = pixel_center;
-            const Vector3d ray_direction = camera_view_direction;
+            // Prepare the ray (origin point and direction)
+            const Vector3d ray_origin = camera_origin;
+            const Vector3d ray_direction = pixel_center - camera_origin;
 
-            // TODO: Check if the ray intersects with the parallelogram
-            if (true)
+            // Check if the ray intersects with the parallelogram
+            if (raytri(pgram_u, pgram_v, ray_direction, pgram_origin, ray_origin))
             {
-                // TODO: The ray hit the parallelogram, compute the exact intersection point
-                Vector3d ray_intersection(0, 0, 0);
+                // The ray hit the parallelogram, compute the exact intersection point
+                Vector3d ray_intersection = getPoint(pgram_u, pgram_v, ray_direction, pgram_origin, ray_origin);
 
-                // TODO: Compute normal at the intersection point
-                Vector3d ray_normal = ray_intersection.normalized();
+                // Compute normal at the intersection point
+                Vector3d ray_normal = pgram_v.cross(pgram_u).normalized();
 
                 // Simple diffuse model
                 C(i, j) = (light_position - ray_intersection).normalized().transpose() * ray_normal;
@@ -198,7 +249,9 @@ void raytrace_shading()
     std::cout << "Simple ray tracer, one sphere with different shading" << std::endl;
 
     const std::string filename("shading.png");
-    MatrixXd C = MatrixXd::Zero(800, 800); // Store the color
+    MatrixXd R = MatrixXd::Zero(800, 800); // Store the red channel
+    MatrixXd G = MatrixXd::Zero(800, 800); // Store the green channel
+    MatrixXd B = MatrixXd::Zero(800, 800); // Store the blue channel
     MatrixXd A = MatrixXd::Zero(800, 800); // Store the alpha mask
 
     const Vector3d camera_origin(0, 0, 3);
@@ -209,11 +262,11 @@ void raytrace_shading()
     const Vector3d x_displacement(2.0 / A.cols(), 0, 0);
     const Vector3d y_displacement(0, -2.0 / A.rows(), 0);
 
-    //Sphere setup
+    // Sphere setup
     const Vector3d sphere_center(0, 0, 0);
     const double sphere_radius = 0.9;
 
-    //material params
+    // Material parameters
     const Vector3d diffuse_color(1, 0, 1);
     const double specular_exponent = 100;
     const Vector3d specular_color(0., 0, 1);
@@ -222,35 +275,42 @@ void raytrace_shading()
     const Vector3d light_position(-1, 1, 1);
     double ambient = 0.1;
 
-    for (unsigned i = 0; i < C.cols(); ++i)
+    for (unsigned i = 0; i < R.cols(); ++i)
     {
-        for (unsigned j = 0; j < C.rows(); ++j)
+        for (unsigned j = 0; j < R.rows(); ++j)
         {
             const Vector3d pixel_center = image_origin + double(i) * x_displacement + double(j) * y_displacement;
 
-            // TODO: Prepare the ray (origin point and direction)
-            const Vector3d ray_origin = pixel_center;
-            const Vector3d ray_direction = camera_view_direction;
+            // Prepare the ray (origin point and direction)
+            const Vector3d ray_origin = camera_origin;
+            const Vector3d ray_direction = pixel_center - camera_origin;
 
             // Intersect with the sphere
-            // TODO: implement the generic ray sphere intersection
-            if (true)
+            if (raysphere(sphere_center, ray_origin, ray_direction, sphere_radius))
             {
-                // TODO: The ray hit the sphere, compute the exact intersection point
-                Vector3d ray_intersection(0, 0, 0);
+                // The ray hit the sphere, compute the exact intersection point
+                Vector3d ray_intersection = getPointSphere(sphere_center, ray_origin, ray_direction, sphere_radius);
 
-                // TODO: Compute normal at the intersection point
-                Vector3d ray_normal = ray_intersection.normalized();
+                // Compute normal at the intersection point
+                Vector3d ray_normal = ((sphere_center - ray_intersection) / sphere_radius).normalized();
 
-                // TODO: Add shading parameter here
-                const double diffuse = (light_position - ray_intersection).normalized().dot(ray_normal);
-                const double specular = (light_position - ray_intersection).normalized().dot(ray_normal);
+                // Add shading parameters
+                Vector3d l = (ray_intersection - light_position).normalized();
+                Vector3d v = (ray_intersection - ray_origin).normalized();
+                Vector3d h = ((v + l) / (v + l).norm()).normalized();
 
-                // Simple diffuse model
-                C(i, j) = ambient + diffuse + specular;
+                Vector3d diffuse = diffuse_color * fmax(0, ray_normal.dot(l));
+                Vector3d specular = specular_color * pow(fmax(0, ray_normal.dot(h)), specular_exponent);
+
+                // Combine shading components
+                R(i, j) = ambient + diffuse(0) + specular(0);
+                G(i, j) = ambient + diffuse(1) + specular(1);
+                B(i, j) = ambient + diffuse(2) + specular(2);
 
                 // Clamp to zero
-                C(i, j) = std::max(C(i, j), 0.);
+                R(i, j) = std::max(R(i, j), 0.);
+                G(i, j) = std::max(G(i, j), 0.);
+                B(i, j) = std::max(B(i, j), 0.);
 
                 // Disable the alpha mask for this pixel
                 A(i, j) = 1;
@@ -259,7 +319,7 @@ void raytrace_shading()
     }
 
     // Save to png
-    write_matrix_to_png(C, C, C, A, filename);
+    write_matrix_to_png(R, G, B, A, filename);
 }
 
 int main()
